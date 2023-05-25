88474fbbb7434a0d3ececb93426619ec3e1a0867
guilhermeonrails <guilhermelimaruby@gmail.com>
2023-05-25 20:10:30 -0300

    video2.2

---
+++ Models/Music.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace screensound_04.Models
+{
+	public class Music
+	{
+        public string? Artist { get; set; }
+        public string? Song { get; set; }
+        public int Duration_ms { get; set; }
+        public bool Explicit { get; set; }
+        public int Year { get; set; }
+        public int Popularity { get; set; }
+        public int Key { get; set; }
+        public double Tempo { get; set; }
+        public string? Genre { get; set; }
+
+        public void ShowDetail()
+        {
+            Console.WriteLine($"Artista: {Artist}");
+            Console.WriteLine($"Música: {Song}");
+            Console.WriteLine($"Duração em minutos: {Duration_ms / 1000}");
+            Console.WriteLine($"18+: {Explicit}");
+            Console.WriteLine($"Ano: {Year}");
+            Console.WriteLine($"Popularidade: {Popularity}");
+            Console.WriteLine($"Tempo: {Tempo}");
+            Console.WriteLine($"Gênero: {Genre}");
+        }
+
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using screensound_04.Models;
+using (HttpClient client = new HttpClient())
+{
+    try
+    {
+        HttpResponseMessage res = await client.GetAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+        res.EnsureSuccessStatusCode();
+        string resBody = await res.Content.ReadAsStringAsync();
+        var songs = JsonConvert.DeserializeObject<List<Music>>(resBody)!;
+        songs[1998].ShowDetail();
+    }
+    catch ( Exception ex)
+    {
+        Console.WriteLine($"Temos um problema: {ex.Message}");
+    }
+}
+
